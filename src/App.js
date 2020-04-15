f910df9217201ba0ae54b301bee106d375fa2a17
judelfino <juscie@gmail.com>
2020-04-15 11:23:22 -0500

    front-end/reactJS, uso de estado e imutabilidade)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
@@ -6,18 +6,53 @@ import Header from './components/Header';
 //primeiro componente, um componete sempre retorna um HTML
 //Tudo no react é um componete
 function App() {
+
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    //adicionar projeto ao array de projects com "push"
+    //projects.push(`Novo projeto ${Date.now()}`);
+
+    //adicionar o conceito de imutabilidade, recriar a varial com o seu contéudo e aicionar o novo
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    //console.log(projects)
+  }
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
-      <Header title="Projects" />
+      <Header title="Homepage" />
+      <ul>
+        { projects.map(project => <li key={ project }> { project } </li>) }
+      </ul>
+
+
+      <button type="button" onClick={ handleAddProject }>Adicionar projeto</button>
     </>
   );
 }
 
 export default App;
+//uso do map() para pecorrer a variavel,sempre retornar algo
+
+/**
+ * 3 forma de retorno
+ *
+ * 1.
+ * <ul>
+        { projects.map(project => {
+          return ...;
+        } )}
+    </ul>
+
+    2.
+    <ul>
+        { projects.map(project => (
+          ...colcoar o conteúdo aqui
+        ) )}
+    </ul>
+
+    3. quando for só uma linha
+
+      <ul>
+        { projects.map(project => <li> {project} </li>)}
+    </ul>
+ */
